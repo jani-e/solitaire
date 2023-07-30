c53ee271865f95b1161468ed335b996f7947b561
jani-e <j.eriksson91@gmail.com>
2023-07-30 16:52:45 +0300

    Added card value logic and reveal/hide state

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 import { useDrag } from 'react-dnd'
 
 const ItemTypes = {
-  CARD: 'card'
+  CARD: 'CARD'
 }
 
 const Card = ({ value, suit, color, tempSuit }) => {
+  const [revealed, setRevealed] = useState(true)
+
   const [{ isDragging }, drag] = useDrag(() => ({
     type: ItemTypes.CARD,
     collect: monitor => ({
@@ -20,12 +23,32 @@ const Card = ({ value, suit, color, tempSuit }) => {
     margin: 5
   }
 
+  const cardValue = () => {
+    switch (value) {
+      case 1: return 'A'
+      case 11: return 'J'
+      case 12: return 'Q'
+      case 13: return 'K'
+      default: return value
+    }
+  }
+
+  if (!revealed) {
+    return (
+      <div ref={drag} style={cardStyle} onClick={() => setRevealed(!revealed)}>
+        <svg width={100} height={150} >
+          <rect width='100%' height='150%' fill="lightgrey" />
+        </svg>
+      </div>
+    )
+  }
+
   return (
-    <div ref={drag} style={cardStyle}>
+    <div ref={drag} style={cardStyle} onClick={() => setRevealed(!revealed)}>
       <svg width={100} height={150} >
         <rect width='100%' height='150%' fill="white" />
-        <text x={80} y={20} fill={color}>{value}</text>
-        <text x={5} y={140} fill={color}>{value}</text>
+        <text x={80} y={20} fill={color}>{cardValue()}</text>
+        <text x={5} y={140} fill={color}>{cardValue()}</text>
         <text x={5} y={20} fill={color}>{suit}</text>
         <text x={80} y={140} fill={color}>{suit}</text>
         <text x={12} y={105} fill={color} fontSize={77}>{tempSuit}</text>
