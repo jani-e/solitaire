63550a917e2b303ebd6c04c1362e25edf10dc9a0
jani-e <j.eriksson91@gmail.com>
2023-08-20 21:04:00 +0300

    Updated stack displaying

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -3,7 +3,9 @@ import Card from "src/components/Card"
 
 const Stack = ({ cards }) => {
   if (cards.length === 0) {
-    return null
+    return (
+      <div></div>
+    )
   }
 
   const [parent, ...children] = cards
@@ -16,7 +18,7 @@ const Stack = ({ cards }) => {
   return (
     <div>
       {<Card value={parent.value} suit={parent.suit} />}
-      <div style={childrenStyle}>{<Stack cards={children} />}</div>
+      {!!children.length && <div style={childrenStyle}>{<Stack cards={children} />}</div>}
     </div>
   )
 }
